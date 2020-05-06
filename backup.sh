49fc1e4d234a55a4934924d6cfd2e6d610fe35f6
AngelBlinder <angelgarcan@blinder.com>
2020-05-06 05:50:19 +0000

    Wed May  6 05:50:19 UTC 2020

---
+++ backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+while(true); do
+    NOW=$(date)
+    echo ===== $NOW
+    #tar -zcvf B5_Clasificacion.tgz B5_Clasificacion/
+    #md5sum B5_Clasificacion.tgz
+    #git config --global user.email "angelgarcan@blinder.com"
+    #git config --global user.name "AngelBlinder"
+    #git add B5_Clasificacion.tgz
+    git add .
+    git commit -a -m "$NOW"
+    git push
+    echo "Sleeping..."
+    echo ""
+    sleep 60
+done
