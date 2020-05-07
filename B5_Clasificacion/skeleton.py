c8e440bd205fda479001cf236e6903e41cd8bb3d
AngelBlinder <angelgarcan@blinder.com>
2020-05-07 07:53:00 +0000

    2020-05-07T07:53:00: B5_Clasificacion

--- B5_Clasificacion/skeleton.py
+++ B5_Clasificacion/skeleton.py
@@ -190,8 +190,12 @@ class kNN:
             vnorm=np.linalg.norm(samples,axis=1)
             samples=samples/vnorm.reshape(len(vnorm),1)
         neighbors,n_ids=self.index.search(samples,self.k)
-        #print(np.max(n_ids))
-        #print(np.min(n_ids))
+#         print(neighbors.shape)
+#         print(neighbors[:3])
+#         print(n_ids.shape)
+#         print(n_ids[:3])
+#         print(np.max(n_ids))
+#         print(np.min(n_ids))
         labels=[np.argmax(np.bincount(self.labels[n_id])) for n_id in n_ids]
         return np.array(labels)
     
