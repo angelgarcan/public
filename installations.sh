0879a828cee31585f5efe9e8ee9db49a61de41de
AngelBlinder <angelgarcan@blinder.com>
2020-05-11 02:23:53 +0000

    2020-05-11T02:23:53: B6_ProyectoIntegrador

--- installations.sh
+++ installations.sh
@@ -11,6 +11,6 @@ conda update --yes -n base conda
 conda install --yes matplotlib pandas numpy scikit-learn nltk jupyternotify
 conda install --yes -c ingeotec microtc
 
-conda update --all
+conda update --yes --all
 
 set +x
