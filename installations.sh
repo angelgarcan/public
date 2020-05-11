9f415d8ce36ccb81e196ce97cb9d64c876ebed27
AngelBlinder <angelgarcan@blinder.com>
2020-05-11 02:08:45 +0000

    2020-05-11T02:08:45: B6_ProyectoIntegrador

--- installations.sh
+++ installations.sh
@@ -3,5 +3,5 @@
 pip install mkl
 pip install faiss-cpu
 
-conda install --yes matplotlib pandas numpy scikit-learn nltk
+conda install --yes matplotlib pandas numpy scikit-learn nltk jupyternotify
 conda install --yes -c ingeotec microtc
