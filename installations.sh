c868a7e9db038205b515f7da36f76e3402527521
AngelBlinder <angelgarcan@blinder.com>
2020-05-11 03:49:31 +0000

    2020-05-11T03:49:31: B6_ProyectoIntegrador

--- installations.sh
+++ installations.sh
@@ -4,11 +4,12 @@ set -x
 # PIP
 pip install mkl
 pip install faiss-cpu
+pip install fasttext==0.8.3
 
 # CONDA
 conda update --yes -n base conda
 
-conda install --yes matplotlib pandas numpy scikit-learn nltk jupyternotify wordcloud gensim
+conda install --yes matplotlib pandas numpy scikit-learn nltk jupyternotify wordcloud gensim=3.8.1
 conda install --yes -c ingeotec microtc
 
 conda update --yes --all
